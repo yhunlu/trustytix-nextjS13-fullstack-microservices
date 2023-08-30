04957b40e6cf0e9743a975e60490bdfff7d497d5
yhunlu <yhunlu87@gmail.com>
2023-08-31 00:41:41 +0300

    add serializeError function in ErrorHandiling.

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,10 +1,21 @@
 import { ValidationError } from 'express-validator';
 
 export class RequestValidationError extends Error {
+  statusCode = 400;
   constructor(public error: ValidationError[]) {
     super();
 
     // extending a built in class
     Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+
+  serializeErrors() {
+    return this.error.map((err) => {
+      if (err.type === 'field') {
+        return { message: err.msg, field: err.path };
+      }
+
+      return { message: err.msg };
+    });
   }
 }
