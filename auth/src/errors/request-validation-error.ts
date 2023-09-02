9686e1b0604f3fd82e13e094256a9cce33d8bd78
yhunlu <yhunlu87@gmail.com>
2023-09-03 01:59:13 +0300

    type mistake

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,7 +1,7 @@
 import { ValidationError } from 'express-validator';
 import { CustomError } from './custom-error';
 
-export class RequestValidationError extends Error {
+export class RequestValidationError extends CustomError {
   statusCode = 400;
   constructor(public error: ValidationError[]) {
     super();
