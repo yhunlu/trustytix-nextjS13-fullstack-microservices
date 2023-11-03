a46a877e33f5077001ee0bc205158544a6658c49
yhunlu <yhunlu87@gmail.com>
2023-11-03 21:05:09 +0300

    test environment setup

---
+++ auth/src/app.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import {
+  currentUserRouter,
+  signinRouter,
+  signoutRouter,
+  signupRouter,
+} from './routes';
+import { errorHandler } from './middlewares';
+import { NotFoundError } from './errors';
+import cookieSession from 'cookie-session';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signupRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
