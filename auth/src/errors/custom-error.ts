d7c6fb90e439d767780a8bf6215958f5b6fddf84
yhunlu <yhunlu87@gmail.com>
2023-11-22 15:59:12 +0300

    implement: bootstrap

--- auth/src/errors/custom-error.ts
+++ auth/src/errors/custom-error.ts
@@ -1,9 +1,17 @@
+/*
+ * CustomError class represents a custom error object.
+ */
 export abstract class CustomError extends Error {
+  /*
+   * Constructs a new instance of the CustomError class.
+   */
   abstract statusCode: number;
 
   constructor() {
+    // Call the parent constructor
     super();
 
+    // Set the prototype of this object to CustomError.prototype
     Object.setPrototypeOf(this, CustomError.prototype);
   }
 
--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,16 +1,29 @@
 import { Request, Response, NextFunction } from 'express';
 import { CustomError } from '../errors';
 
+/*
+ *
+ * Error handler middleware.
+ *
+ * @param err - The error object.
+ * @param req - The request object.
+ * @param res - The response object.
+ * @param next - The next function.
+ */
+
 export const errorHandler = (
   err: Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
+  // Check if the error is an instance of CustomError
   if (err instanceof CustomError) {
+    // If it is, send the serialized errors with the corresponding status code
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
+  // If the error is not an instance of CustomError, send a generic error message with status code 400
   res.status(400).send({
     errors: [{ message: 'Something went wrong' }],
   });
